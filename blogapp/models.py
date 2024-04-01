8f1c457426289672225552d25bafbab908e6305e
razbudin <razbudin@yandex.ru>
2024-04-02 01:20:20 +0300

    5.6 Главная, посты и пагинация

--- blogapp/models.py
+++ blogapp/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     h1 = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     url = models.SlugField()
-    description = RichTextUploadingField()
+    description = models.TextField()
     content = RichTextUploadingField()
     image = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
 
@@ -6,5 +7,10 @@ from blogapp.models import Post
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        posts = Post.objects.all()
-        return render(request, 'blogapp/index.html', {'posts': posts})
+        posts = Post.objects.all().order_by('-created_at')
+        paginator = Paginator(posts, 3)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'blogapp/index.html', context={'page_obj': page_obj})
