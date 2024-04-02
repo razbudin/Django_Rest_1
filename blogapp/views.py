d4a3e5b3ef8478bed6c62f72547b9a4ed8bcb7e0
razbudin <razbudin@yandex.ru>
2024-04-02 14:07:47 +0300

    5.7 Страница поста

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from blogapp.models import Post
@@ -14,3 +14,9 @@ class MainView(View):
         page_obj = paginator.get_page(page_number)
 
         return render(request, 'blogapp/index.html', context={'page_obj': page_obj})
+
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'blogapp/post_detail.html', context={'post':post})
